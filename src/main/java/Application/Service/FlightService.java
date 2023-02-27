333ddda4c6c220a6e72747bd49d188fea94b66ea
jarthurj <jaoldaker1@gmail.com>
2023-02-27 02:16:52 +0000

    complete

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,7 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        return this.flightDAO.insertFlight(flight);
     }
 
     /**
@@ -70,7 +70,14 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+        
+        if(this.flightDAO.getFlightById(flight_id) != null){
+            this.flightDAO.updateFlight(flight_id,flight);
+            
+        }else{
+            return null;
+        }
+        return this.flightDAO.getFlightById(flight_id);
     }
 
     /**
@@ -80,7 +87,7 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        return this.flightDAO.getAllFlights();
     }
 
     /**
@@ -92,6 +99,6 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+        return this.flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
     }
 }
